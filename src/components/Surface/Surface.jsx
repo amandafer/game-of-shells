94b4f92a3cc4316b0662e74758d962519553626a
Amanda Fernandes <amandafer6@gmail.com>
2019-07-23 22:45:18 +0100

    Polished hooks

--- src/components/Surface/Surface.jsx
+++ src/components/Surface/Surface.jsx
@@ -9,7 +9,6 @@ import closedShell from '../../assets/closed-shell.png';
 import emptyShell from '../../assets/empty-shell.png';
 import { setResult, setAnimationStop } from '../../actions';
 import { Shell } from '../Shell';
-import { isAbsolute } from 'path';
 
 const style = {
 	position: 'absolute',
@@ -22,6 +21,16 @@ const style = {
 	justifyContent: 'center',
 	alignItems: 'baseline',
 	zIndex: 100
+};
+
+const changeShellImage = (shells, id, image) => {
+	const result = shells.find(shell => shell.id === id);
+
+	if (result) {
+		result.image = image;
+	}
+
+	return [...shells];
 };
 
 const createShells = numberOfShells => {
@@ -40,24 +49,24 @@ const Surface = () => {
 	const [
 		numberOfShells,
 		pearlIndex,
+		canUserGuess,
 		isUserPlaying,
 		shouldAnimate
-	] = useSelector(({ shellReducer, animationReducer }) => [
-		shellReducer.numberOfShells,
-		shellReducer.pearlIndex,
-		shellReducer.isPlaying,
+	] = useSelector(({ gameReducer, animationReducer }) => [
+		gameReducer.numberOfShells,
+		gameReducer.pearlIndex,
+		gameReducer.canUserGuess,
+		gameReducer.isPlaying,
 		animationReducer.shouldAnimate
 	]);
 	const [shells, updateShells] = useState(
 		createShells(numberOfShells, dispatch)
 	);
 	const onClickHandler = id => {
-		if (isUserPlaying) {
+		if (canUserGuess) {
 			updateShells(shells => {
-				const clickedShell = shells.find(shell => shell.id === id);
-				clickedShell.image =
-					pearlIndex === id ? pearlShell : emptyShell;
-				return [...shells];
+				const image = pearlIndex === id ? pearlShell : emptyShell;
+				return changeShellImage(shells, id, image);
 			});
 			dispatch(setResult(id));
 		}
@@ -66,33 +75,36 @@ const Surface = () => {
 	useEffect(() => {
 		let count = 0;
 		if (shouldAnimate) {
+			updateShells(shells =>
+				changeShellImage(shells, pearlIndex, closedShell)
+			);
+
 			const interval = setInterval(() => {
 				updateShells(shuffle(shells));
 				count++;
 				if (count > 5) {
-					dispatch(setAnimationStop);
+					dispatch(setAnimationStop());
 					clearInterval(interval);
 				}
 			}, 500);
 		}
 	}, [shouldAnimate]);
 	useEffect(() => {
-		updateShells(shells => {
-			const clickedShell = shells.find(shell => shell.id === pearlIndex);
-			if (clickedShell)
-				clickedShell.image = shouldAnimate ? closedShell : pearlShell;
-			return [...shells];
-		});
-	}, [pearlIndex, shouldAnimate]);
+		if (isUserPlaying) {
+			updateShells(shells => {
+				shells.map(
+					shell =>
+						(shell.image =
+							shell.id === pearlIndex ? pearlShell : closedShell)
+				);
+
+				return [...shells];
+			});
+		}
+	}, [pearlIndex]);
 
 	return (
-		<FlipMove
-			style={style}
-			staggerDurationBy="30"
-			duration={500}
-			enterAnimation="accordionHorizontal"
-			leaveAnimation="accordionHorizontal"
-		>
+		<FlipMove style={style} staggerDurationBy="30" duration={500}>
 			{shells.map(shell => (
 				<Shell
 					key={shell.id}
